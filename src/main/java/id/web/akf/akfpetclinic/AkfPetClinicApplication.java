d6f83b953a01a68b7024f7bac0dda29d871f77c9
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2018-11-03 05:52:21 +0700

    Initial Project Add

---
+++ src/main/java/id/web/akf/akfpetclinic/AkfPetClinicApplication.java
@@ -0,0 +1,12 @@
+package id.web.akf.akfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkfPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/id/web/akf/akfpetclinic/AkfPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package id.web.akf.akfpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkfPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
