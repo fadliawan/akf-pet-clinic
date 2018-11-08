a04303c31ba56e141af3b4887a5d132af642b773
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2018-11-08 09:08:32 +0700

    Refactor to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/id/web/akf/akfpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/id/web/akf/akfpetclinic/services/OwnerService.java
@@ -2,16 +2,8 @@ package id.web.akf.akfpetclinic.services;
 
 import id.web.akf.akfpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/id/web/akf/akfpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/id/web/akf/akfpetclinic/services/PetService.java
@@ -2,14 +2,6 @@ package id.web.akf.akfpetclinic.services;
 
 import id.web.akf.akfpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
