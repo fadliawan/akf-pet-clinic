6b66193a6d013aab1d4ea4bfa21304a125bd59e5
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2018-11-10 16:45:42 +0700

    Implement ID generation for map services. Closes #23.

--- pet-clinic-web/src/main/java/id/web/akf/akfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/id/web/akf/akfpetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package id.web.akf.akfpetclinic.controllers;
 
+import id.web.akf.akfpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
