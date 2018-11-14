a8e80b079d8e783f4a0fc5062b68c717eaecedc0
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2018-11-14 09:19:01 +0700

    Load PetType data on startup. Closes #32.

--- pet-clinic-web/src/main/java/id/web/akf/akfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/id/web/akf/akfpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package id.web.akf.akfpetclinic.bootstrap;
 
 import id.web.akf.akfpetclinic.model.Owner;
+import id.web.akf.akfpetclinic.model.PetType;
 import id.web.akf.akfpetclinic.model.Vet;
 import id.web.akf.akfpetclinic.services.OwnerService;
+import id.web.akf.akfpetclinic.services.PetTypeService;
 import id.web.akf.akfpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
