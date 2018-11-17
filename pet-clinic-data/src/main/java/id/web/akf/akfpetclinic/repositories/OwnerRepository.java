c2517e07996b48f015df09a330264d3f374024b2
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2018-11-17 19:50:10 +0700

    Add Spring Data JPA service for owner. Closes #40.

--- pet-clinic-data/src/main/java/id/web/akf/akfpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/id/web/akf/akfpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,7 @@ import id.web.akf.akfpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+
 }
